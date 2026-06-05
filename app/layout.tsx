6ed17f12a03144a142251b026dc8800d40c22eca
askohd <askodasti@gmail.com>
2026-06-06 01:15:31 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header';
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata: Metadata = {
   title: 'AskoList – Discover Discord Servers Worldwide',
