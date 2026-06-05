b8fe4abdf844b66c61c4aedd636696102bfe0a19
askohd <askodasti@gmail.com>
2026-06-06 00:29:47 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,30 @@
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="nav">
       <div className="container nav-inner">
         <Link className="logo" href="/">
-          <span className="logo-mark">A</span>
+          <span className="logo-mark">
+            <Image
+              src="/logo.png"
+              alt="AskoList Logo"
+              width={38}
+              height={38}
+              priority
+            />
+          </span>
           <span>AskoList</span>
         </Link>
+
         <nav className="nav-links">
           <Link href="/servers">Servers</Link>
-          <Link href="/submit">Submit</Link>
+          <Link href="/submit">Submit Server</Link>
           <Link href="/shop">Shop</Link>
           <Link href="/admin">Admin</Link>
         </nav>
+
         <button className="btn secondary">Login with Discord</button>
       </div>
     </header>
